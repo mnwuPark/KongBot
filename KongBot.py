784a73e3555844e1c97d260941aa268815f12878
mnwuPark <mw.park@geeksbin.com>
2022-09-16 17:33:37 +0900

    Add bot cilent 1. import discord 2. add bot commands 3. add bot activitys

--- KongBot.py
+++ KongBot.py
@@ -1,1 +1,16 @@
 #Project_By mnwuPark KongBot.py
+import discord
+from discord.ext import commands
+
+token = ""
+
+bot = commands.Bot(command_prefix = ".")
+
+@bot.event
+async def on_ready():
+    print("Login to:", bot.user.name)
+    print("Bot id:", bot.user.id)
+    print("Bot Connection has succesful !")
+    await bot.change_presence(status = discord.status.online, activity = None)
+
+bot.run(token)
